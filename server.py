dcf786d41fc464de561fc77ebeb06ab797b6ac3b
alyssasalem <alyssabsalem@gmail.com>
2021-12-06 20:50:40 +0000

    Created routes to homepage and sign up.

--- server.py
+++ server.py
@@ -4,7 +4,15 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def homepage():
+    """ Show the homepage."""
+    return render_template("homepage.html")
 
+@app.route("/sign-up")
+def sign_up():
+    """ Show sign-up form."""
+    return render_template("sign-up.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
